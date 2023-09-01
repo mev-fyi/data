279a694d9b9e7ace00bacef6205d5701d66353e8
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-09-01 14:21:43 +0200

    add setup script and requirements.txt

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# create virtual environment
+python -m venv venv
+
+# activate virtual environment
+source venv/bin/activate
+
+# install packages from requirements.txt
+pip install -r requirements.txt
+
+# deactivate virtual environment
+deactivate
