b14d4a4e40853b2274ac997004e63e0b11ddb828
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-09-17 15:31:59 +0200

    features: add research paper support for ResearchGate, Nature, DL-ACM

--- src/get_research_paper_details.py
+++ src/get_research_paper_details.py
@@ -1,4 +1,6 @@
 import os
+import random
+import time
 import arxiv
 import requests
 import csv
@@ -8,6 +10,11 @@ import logging
 import re
 from datetime import datetime
 from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 from src.utils import root_directory, ensure_newline_in_csv, read_existing_papers, read_csv_links_and_referrers, paper_exists_in_list, quickSoup
 
@@ -15,6 +22,38 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger('arxiv')
 logger.setLevel(logging.WARNING)
+
+
+# Define the return_driver function
+def return_driver():
+    # set up Chrome driver options
+    options = webdriver.ChromeOptions()
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--start-maximized")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-features=IsolateOrigins,site-per-process")
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+    # driver_version = "112.0.5615.49"
+    # driver_version = "114.0.5735.106"
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    # NOTE: as of 2023-08-24, the ChromeDriverManager().install() no longer works
+    # needed to manually go here https://googlechromelabs.github.io/chrome-for-testing/#stable
+    # and provide direct paths to script for both binary and driver
+    # First run the script get_correct_chromedriver.sh
+    # Paths for the Chrome binary and ChromeDriver
+    CHROME_BINARY_PATH = f'{root_directory()}/src/chromium/chrome-linux64/chrome'
+    CHROMEDRIVER_PATH = f'{root_directory()}/src/chromium/chromedriver-linux64/chromedriver'
+
+    options = webdriver.ChromeOptions()
+    options.binary_location = CHROME_BINARY_PATH
+
+    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
+    return driver
 
 
 def get_paper_details_from_arxiv(arxiv_url: str) -> dict or None:
@@ -128,6 +167,138 @@ def get_paper_details_from_iacr(url: str):
     except AttributeError as e:
         print(f"Failed to parse the paper details: {e}")
         return None
+
+
+def get_paper_details_from_dl_acm(url: str):
+    try:
+        response = requests.get(url.replace('.pdf', ''))
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        paper_title = soup.select_one('.citation__title').get_text()
+
+        # Get Paper Authors
+        # Select all <li> elements with class "loa__item"
+        author_items = soup.select('li.loa__item')
+
+        # Initialize a list to store the extracted author names
+        paper_authors = []
+
+        # Loop through the author items
+        for author_item in author_items:
+            # Find the <img> element within the author item
+            img_element = author_item.select_one('img.author-picture')
+
+            # Extract the author name from the alt attribute of the <img> element
+            if img_element:
+                author_name = img_element.get_text()
+                if author_name:
+                    paper_authors.append(author_name)
+
+        date_string = soup.select_one('.CitationCoverDate').get_text()
+
+        # Parse the date string
+        date_obj = datetime.strptime(date_string, '%d %B %Y')
+
+        # Format the date as yyyy-mm-dd
+        paper_release_date = date_obj.strftime('%Y-%m-%d')
+
+        return {"title": paper_title, "authors": paper_authors, "pdf_link": url, "topics": 'dl-acm', "release_date": paper_release_date}
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to fetch the paper details: {e}")
+        return None
+    except AttributeError as e:
+        print(f"Failed to parse the paper details: {e}")
+        return None
+
+
+def get_paper_details_from_nature(url: str):
+    try:
+        response = requests.get(url.replace('.pdf', ''))
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        try:
+            article_identifier = soup.select_one('.c-article-identifiers__type').get_text()
+        except AttributeError:
+            article_identifier = soup.select_one('li.c-article-identifiers__item:nth-child(1)').get_text()
+
+        if article_identifier == 'COMMENT':
+            paper_title = soup.select_one('.c-article-magazine-title').get_text()
+
+            # Select the .c-article-author-list element
+            author_list_text = soup.select_one('.c-article-author-list').get_text()
+
+            # Remove non-alphabet characters, split the text by commas, and clean up spaces
+            paper_authors = ', '.join([re.sub(r'[^a-zA-Z, ]', '', author.replace('&', ',')).strip() for author in author_list_text.split(',')])
+
+            paper_authors = re.sub(r'\s+', ' ', paper_authors)  # Replace multiple spaces with a single space
+
+            # get published date
+            date_string = soup.select_one('li.c-article-identifiers__item:nth-child(2) > time:nth-child(1)').get_text()
+            date_obj = datetime.strptime(date_string, '%d %B %Y')  # Parse the date string
+            paper_release_date = date_obj.strftime('%Y-%m-%d')  # Format the date as yyyy-mm-dd
+
+        elif article_identifier == 'News & Views':
+            paper_title = soup.select_one('.c-article-title').get_text()
+
+            # Select the .c-article-author-list element
+            author_list_text = soup.select_one('.c-article-author-list').get_text().split('\n')[0]
+
+            # Remove non-alphabet characters, split the text by commas, and clean up spaces
+            paper_authors = ', '.join([re.sub(r'[^a-zA-Z, ]', '', author.replace('&', ',')).strip() for author in author_list_text.split(',')])
+
+            paper_authors = re.sub(r'\s+', ' ', paper_authors)  # Replace multiple spaces with a single space
+
+            date_string = soup.select_one('li.c-article-identifiers__item:nth-child(2) > a:nth-child(1) > time:nth-child(1)').get_text()
+            date_obj = datetime.strptime(date_string, '%d %B %Y')  # Parse the date string
+            paper_release_date = date_obj.strftime('%Y-%m-%d')  # Format the date as yyyy-mm-dd
+
+        return {"title": paper_title, "authors": paper_authors, "pdf_link": url, "topics": 'nature', "release_date": paper_release_date}
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to fetch the paper details: {e}")
+        return None
+    except AttributeError as e:
+        print(f"Failed to parse the paper details: {e}")
+        return None
+
+
+def get_paper_details_from_research_gate(url: str):
+    try:
+        driver = return_driver()
+
+        driver.get(url.replace('.pdf', ''))
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'h1.nova-legacy-e-text')))
+
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+        paper_title = soup.select_one('h1.nova-legacy-e-text').get_text()
+
+        # Loop through the author list
+        authors = soup.select('.nova-legacy-l-flex .research-detail-author-list__item a')
+        paper_authors = [author.get_text().strip() for author in authors if author]
+        # remove empty string, remove duplicates
+        paper_authors = list(dict.fromkeys(filter(None, paper_authors)))
+        # Try some manual cleaning and see if that goes through
+        paper_authors = [author for author in paper_authors if not re.search(r'university|institute|school', author, flags=re.IGNORECASE)]
+
+        date_string = soup.select_one('div.nova-legacy-e-text--spacing-xxs:nth-child(1) > ul:nth-child(1) > li:nth-child(1)').get_text()
+
+        # Parse the date string
+        date_obj = datetime.strptime(date_string, '%B %Y')
+
+        # Format the date as yyyy-mm-dd
+        paper_release_date = date_obj.strftime('%Y-%m-%d')
+
+        return {"title": paper_title, "authors": paper_authors, "pdf_link": url, "topics": 'ResearchGate', "release_date": paper_release_date}
+    except TimeoutException as e:
+        print(f"Timeout waiting for page to load: {e}")
+        return None
+    except Exception as e:
+        print(f"Failed to fetch or parse the paper details: {e}")
+        return None
+    finally:
+        driver.quit()
 
 
 def download_and_save_unique_paper(args):
@@ -265,4 +436,29 @@ if __name__ == "__main__":
             parsing_method=get_paper_details_from_iacr
         )
 
+        # For DL-ACM links
+        dl_acm_links_and_referrers = read_csv_links_and_referrers(os.path.join(root_directory(), 'data', 'links', 'research_papers/dl.acm_papers.csv'))
+        download_and_save_paper(
+            paper_site='DL-ACM',
+            paper_links_and_referrers=dl_acm_links_and_referrers,
+            csv_file=csv_file,
+            parsing_method=get_paper_details_from_dl_acm
+        )
 
+        # For nature links
+        nature_links_and_referrers = read_csv_links_and_referrers(os.path.join(root_directory(), 'data', 'links', 'research_papers/nature_papers.csv'))
+        download_and_save_paper(
+            paper_site='Nature',
+            paper_links_and_referrers=nature_links_and_referrers,
+            csv_file=csv_file,
+            parsing_method=get_paper_details_from_nature
+        )
+
+        # For ResearchGate links
+        research_gate_links_and_referrers = read_csv_links_and_referrers(os.path.join(root_directory(), 'data', 'links', 'research_papers/researchgate_papers.csv'))
+        download_and_save_paper(
+            paper_site='Research Gate',
+            paper_links_and_referrers=research_gate_links_and_referrers,
+            csv_file=csv_file,
+            parsing_method=get_paper_details_from_research_gate
+        )
