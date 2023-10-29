422e22a374f1df65d233489d8e8e025f66b643ee
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-10-29 12:59:25 +0100

    feature: add more keywords to filter for

--- src/populate_csv_files/constants.py
+++ src/populate_csv_files/constants.py
@@ -7,7 +7,8 @@ KEYWORDS_TO_INCLUDE = ['order flow', 'orderflow', 'transaction', 'mev', 'orderin
             'altlayer', 'tarun', 'modular summit', 'latency', 'market design', 'searcher', 'staking', 'pre-merge', 'post-merge',
             'liquid staking', 'crediblecommitments', 'tee', 'market microstructure', 'rollups', 'uniswap', '1inch',
             'cow', 'censorship', 'liquidity', 'censorship', 'ofa', 'pfof', 'payment for order flow', 'decentralisation', 'decentralization',
-            'erc', 'eip', 'auction', 'daian', 'mechanism design', 'Price-of-Anarchy', 'protocol economics', 'stephane gosselin', 'su zhu', 'pools', 'censorship']
+            'erc', 'eip', 'auction', 'daian', 'mechanism design', 'Price-of-Anarchy', 'protocol economics', 'stephane gosselin', 'su zhu', 'pools', 'censorship',
+            '1559', 'BFT', 'selfish mining', 'vickrey auctions']
 
 # , 'smart contract', 'eth global',  'evm',  #  'vitalik', 'buterin', bridge',
 KEYWORDS_TO_EXCLUDE = ['DAO', 'NFTs', 'joke', 'jokes', 'short', 'shorts', '#', 'gensler', 'sec']
