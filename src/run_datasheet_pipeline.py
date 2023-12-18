93bae111bbed273853e31a7b425da3d7ebba8879
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-12-18 16:52:09 +0100

    data upload: medium articles

--- src/run_datasheet_pipeline.py
+++ src/run_datasheet_pipeline.py
@@ -3,7 +3,7 @@ from src import get_research_paper_details, update_google_sheet
 from src.populate_csv_files import pretty_print_articles, fetch_youtube_video_details_from_handles
 
 fetch_youtube_video_details_from_handles.run()
-populate_csv_files.parse_new_data.run()
-pretty_print_articles.run()
-get_research_paper_details.main()
+# populate_csv_files.parse_new_data.run()
+# pretty_print_articles.run()
+# get_research_paper_details.main()
 update_google_sheet.main()
