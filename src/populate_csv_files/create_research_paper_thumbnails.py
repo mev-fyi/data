1931a89b119c79f91f9328c4bff798e418e2f6d3
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-12-28 23:34:37 +0100

    feat: update research paper thumbnails with zooming

--- src/populate_csv_files/create_research_paper_thumbnails.py
+++ src/populate_csv_files/create_research_paper_thumbnails.py
@@ -5,10 +5,17 @@ from io import BytesIO
 from src.utils import root_directory
 
 
-def generate_pdf_thumbnails(pdf_directory, output_directory, page_fraction=0.4, min_px=600, max_px=900):
+def generate_pdf_thumbnails(pdf_directory, output_directory, page_fraction=0.5, min_px=400, max_px=1500, zoom_factor=1.3):
     """
-    Generate thumbnails for the top portion of the first page of all PDFs in the pdf_directory.
-    Thumbnails will have a size between min_px and max_px on the longest side.
+    Generate thumbnails for the top portion of the first page of all PDFs in the pdf_directory, then apply zooming.
+    Thumbnails will have a size between min_px and max_px on the longest side after zooming.
+    Args:
+        pdf_directory (str): Directory containing PDF files.
+        output_directory (str): Directory to save the thumbnails.
+        page_fraction (float): Fraction of the page to be shown in the thumbnail (0 to 1).
+        min_px (int): Minimum pixel size for the thumbnail's longest side.
+        max_px (int): Maximum pixel size for the thumbnail's longest side.
+        zoom_factor (float): Factor by which to zoom into the cropped thumbnail.
     """
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
@@ -21,20 +28,30 @@ def generate_pdf_thumbnails(pdf_directory, output_directory, page_fraction=0.4, 
             with fitz.open(pdf_path) as doc:
                 page = doc[0]  # First page
 
-                # Calculate zoom factors based on the desired pixel size and actual page size
-                scale = min_px / min(page.rect.width, page.rect.height)
-                if scale * max(page.rect.width, page.rect.height) > max_px:
-                    scale = max_px / max(page.rect.width, page.rect.height)
-
-                mat = fitz.Matrix(scale, scale)  # Create the matrix with the zoom scale
+                # Calculate initial scale to fit the min_px requirement
+                initial_scale = min_px / min(page.rect.width, page.rect.height * page_fraction)
+                mat = fitz.Matrix(initial_scale, initial_scale)  # Create the matrix with the initial scale
                 pix = page.get_pixmap(matrix=mat)  # Render page to an image
                 img = Image.open(BytesIO(pix.tobytes()))  # Convert to a PIL image
 
                 # Crop the image to the desired page_fraction
                 img_cropped = img.crop((0, 0, img.width, int(img.height * page_fraction)))
 
-                # Save the thumbnail
-                img_cropped.save(thumbnail_path)
+                # Calculate the final size with zoom factor
+                final_width = int(img_cropped.width * zoom_factor)
+                final_height = int(img_cropped.height * zoom_factor)
+
+                # Ensure final dimensions are within the min_px and max_px bounds
+                if final_width > max_px or final_height > max_px:
+                    scale_factor = min(max_px / final_width, max_px / final_height)
+                    final_width = int(final_width * scale_factor)
+                    final_height = int(final_height * scale_factor)
+
+                # Resize image to final dimensions
+                img_final = img_cropped.resize((final_width, final_height), Image.Resampling.LANCZOS)
+
+                # Save the final thumbnail
+                img_final.save(thumbnail_path)
                 print(f"Thumbnail generated for {filename} at {thumbnail_path}")
 
 
