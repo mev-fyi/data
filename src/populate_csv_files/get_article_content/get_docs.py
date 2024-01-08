b3d10503e42309ab4543e5b9239c43933d184cc7
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-01-08 02:50:53 +0100

    feat: get_docs: process videos in parallel, enable parameter to exclude direcotires, add methods to alter the HTML e.g. remove_images

--- src/populate_csv_files/get_article_content/get_docs.py
+++ src/populate_csv_files/get_article_content/get_docs.py
@@ -6,7 +6,7 @@ import time
 from markdown import markdown
 from src.utils import root_directory
 import dotenv
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from weasyprint import HTML
 
 
@@ -31,23 +31,44 @@ def get_content_list(github_api_url, headers, retry_count=3, delay=5):
         logging.error(f"Error fetching content from {github_api_url}: {e}")
         return []
 
-def process_directory(output_dir, api_url, headers, overwrite=True):
+def process_directory(output_dir, api_url, headers, overwrite=True, exclude_dirs=None):
+    # Check if the current directory is in the exclude list
+    if exclude_dirs and any(ex_dir in output_dir for ex_dir in exclude_dirs):
+        logging.info(f"Skipping excluded directory: {output_dir}")
+        return
+
     contents = get_content_list(api_url, headers)
+    if not contents:
+        logging.error(f"No contents found at API URL: {api_url}")
+        return
+
+    logging.info(f"Processing directory: {output_dir}")
+
     with ThreadPoolExecutor() as executor:
         futures = []
+
         for content in contents:
             if content['type'] == 'file' and content['name'].endswith(('.md', '.mdx')):
                 future = executor.submit(process_file, output_dir, content, headers, overwrite)
                 futures.append(future)
-            elif content['type'] == 'dir':
-                new_output_dir = os.path.join(output_dir, content['name'])
-                new_api_url = content['url']
-                os.makedirs(new_output_dir, exist_ok=True)
-                process_directory(new_output_dir, new_api_url, headers, overwrite)  # Nested directories are processed sequentially
 
-        # Wait for all futures to complete
-        for future in futures:
-            future.result()
+        for future in as_completed(futures):
+            try:
+                future.result()
+            except Exception as e:
+                logging.error(f"Error processing a file: {e}")
+
+    # Process subdirectories sequentially
+    for content in contents:
+        if content['type'] == 'dir':
+            new_output_dir = os.path.join(output_dir, content['name'])
+            new_api_url = content['url']
+            os.makedirs(new_output_dir, exist_ok=True)
+            logging.info(f"Processing subdirectory: {content['name']}")
+            process_directory(new_output_dir, new_api_url, headers, overwrite, exclude_dirs)
+
+    # logging.info(f"Finished processing directory: {output_dir}")
+
 
 
 def process_file(output_dir, file_info, headers, overwrite):
@@ -55,7 +76,7 @@ def process_file(output_dir, file_info, headers, overwrite):
     output_pdf_path = os.path.join(output_dir, file_name.replace('.md', '.pdf').replace('.mdx', '.pdf'))
 
     if not overwrite and os.path.exists(output_pdf_path):
-        logging.info(f"File already exists: {output_pdf_path}. Skipping download and conversion.")
+        # logging.info(f"File already exists: {output_pdf_path}. Skipping download and conversion.")
         return
 
     try:
@@ -66,24 +87,25 @@ def process_file(output_dir, file_info, headers, overwrite):
     except Exception as e:
         logging.error(f"Error processing file {file_name}: {e}")
 
+
 def convert_to_pdf_with_fallback(markdown_content, output_pdf_path, retry_count=3, delay=5):
     # First try using pdfkit
     try:
-        html_content = '<meta charset="UTF-8">' + markdown(markdown_content, extensions=['md_in_html'])
+        html_content = prepare_html_content(markdown_content)
         options = {
             'encoding': "UTF-8",
             'custom-header': [('Content-Encoding', 'utf-8')],
             'no-outline': None
         }
         pdfkit.from_string(html_content, output_pdf_path, options=options)
-        return True  # Successfully converted using pdfkit
+        return True
     except Exception as e:
         logging.warning(f"pdfkit failed to convert to PDF: {e}. Trying WeasyPrint...")
 
     # If pdfkit fails, try using WeasyPrint
     try:
         HTML(string=html_content).write_pdf(output_pdf_path)
-        return True  # Successfully converted using WeasyPrint
+        return True
     except Exception as e:
         if retry_count > 0:
             logging.warning(f"WeasyPrint also failed to convert to PDF: {e}. Retrying in {delay} seconds...")
@@ -91,35 +113,66 @@ def convert_to_pdf_with_fallback(markdown_content, output_pdf_path, retry_count=
             return convert_to_pdf_with_fallback(markdown_content, output_pdf_path, retry_count-1, delay*2)
 
     logging.error(f"Failed to convert to PDF after retries: {output_pdf_path}")
-    return None  # Failed conversion in all attempts
+    return None
 
 
-def fetch_and_save_as_pdf(output_dir, repo_api_url, headers, overwrite=True):
-    try:
-        os.makedirs(output_dir, exist_ok=True)
-        process_directory(output_dir, repo_api_url, headers, overwrite)
-    except Exception as e:
-        logging.error(f"Error fetching and saving as PDF: {e}")
+def prepare_html_content(markdown_content):
+    # Convert Markdown to HTML
+    html_content = '<meta charset="UTF-8">' + markdown(markdown_content, extensions=['md_in_html'])
+    # Remove or modify image tags
+    html_content = remove_images(html_content)
+    return html_content
+
+def remove_images(html_content):
+    """
+    Removes or modifies image tags in HTML content.
+
+    This function parses the given HTML content and removes all image tags
+    (`<img>`). This is a crucial step to avoid errors during PDF conversion
+    caused by image tags. These errors can occur due to various reasons such
+    as relative image paths not resolving correctly in the context of the PDF
+    generator, missing image files, or unsupported URI schemes in the `src`
+    attribute of image tags.
+
+    Parameters:
+    html_content (str): The HTML content from which images are to be removed.
+
+    Returns:
+    str: HTML content with image tags removed or modified.
+
+    Note:
+    The function uses BeautifulSoup for parsing and manipulating the HTML
+    content. Image tags are completely removed in this implementation, but
+    could be replaced with alternative text or placeholders if desired.
+    """
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+    for img in soup.find_all('img'):
+        img.decompose()  # This will remove the image tag completely
+    return str(soup)
+
+
+def fetch_and_save_as_pdf(output_dir, repo_api_url, headers, exclude_dirs, overwrite=True):
+    # No need to pass executor as an argument here
+    process_directory(output_dir, repo_api_url, headers, overwrite, exclude_dirs)
 
 def process_repositories(overwrite=True):
     headers = {'Authorization': f'token {GITHUB_TOKEN}'} if GITHUB_TOKEN else {}
-    if GITHUB_TOKEN:
-        logging.info("Github token found! Authenticated to increase number of get requests")
-    else:
-        logging.warning('Github token not provided, defaulting to unauthenticated get requests')
+    exclude_dirs = ['translations']  # List of directories to exclude
 
     # Flashbots Docs
     # flashbots_output_dir = f'{root_directory()}/data/flashbots_docs_pdf/'
     # flashbots_repo_api_url = "https://api.github.com/repos/flashbots/flashbots-docs/contents/docs"
-    # fetch_and_save_as_pdf(flashbots_output_dir, flashbots_repo_api_url)
+    # fetch_and_save_as_pdf(flashbots_output_dir, flashbots_repo_api_url, headers, overwrite)
 
     # Ethereum Org Website
     ethereum_output_dir = f'{root_directory()}/data/ethereum_org_website_content/'
     ethereum_repo_api_url = "https://api.github.com/repos/ethereum/ethereum-org-website/contents/src/content"
-    fetch_and_save_as_pdf(ethereum_output_dir, ethereum_repo_api_url, headers, overwrite)
+
+    fetch_and_save_as_pdf(ethereum_output_dir, ethereum_repo_api_url, headers, overwrite, exclude_dirs)
 
 if __name__ == "__main__":
     try:
-        process_repositories(overwrite=False) # Change to False to avoid overwriting
+        process_repositories(overwrite=False)
     except Exception as e:
         logging.error(f"Error processing repositories: {e}")
