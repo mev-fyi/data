910853002b19bc3d0eead5b65cae7908ee3b4f1e
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-01-23 21:29:01 +0100

    feat: add create_research_paper_thumbnails back to run_datasheet_pipeline.py

--- src/run_datasheet_pipeline.py
+++ src/run_datasheet_pipeline.py
@@ -1,14 +1,16 @@
 import populate_csv_files.parse_new_data
 from src import get_research_paper_details, update_google_sheet
-from src.populate_csv_files import pretty_print_articles, fetch_youtube_video_details_from_handles, create_articles_thumbnails
+from src.populate_csv_files import pretty_print_articles, fetch_youtube_video_details_from_handles, create_articles_thumbnails, create_research_paper_thumbnails
 from src.populate_csv_files.get_article_content import get_article_content
+
 
 # fetch_youtube_video_details_from_handles.run()
 populate_csv_files.parse_new_data.run()  # parse incoming data
-# pretty_print_articles.run()  # get article titles
-# get_article_content.run(overwrite=True)  # get article content
+pretty_print_articles.run()  # get article titles
+get_article_content.run(overwrite=True)  # get article content
 
-# create_articles_thumbnails.main(headless=True)  # get article thumbnails
+# create_articles_thumbnails.main(headless=False)  # get article thumbnails
+# create_research_paper_thumbnails.run()
 
-# get_research_paper_details.main()  # get research article details
-# update_google_sheet.main()
+get_research_paper_details.main()  # get research article details
+update_google_sheet.main()
