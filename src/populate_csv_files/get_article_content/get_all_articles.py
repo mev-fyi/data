ffd8d7fe4b2e7ec51a0cac0d097e3d92fa461af0
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-03-01 14:52:03 +0100

    refactor: enable to process single .csv file of discourse forum

--- src/populate_csv_files/get_article_content/get_all_articles.py
+++ src/populate_csv_files/get_article_content/get_all_articles.py
@@ -10,21 +10,24 @@ from concurrent.futures import ThreadPoolExecutor
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def process_single_csv(file, csv_dir, output_dir):
-    df = pd.read_csv(os.path.join(csv_dir, file))
-    output_subdir = os.path.join(output_dir, file.split('.')[0])
+def process_single_csv(file_path, output_dir, overwrite=False):
+    file_name = os.path.basename(file_path)
+    logging.info(f"Started processing [{file_name}]")
+    df = pd.read_csv(file_path)
+    output_subdir = os.path.join(output_dir, file_name.split('.')[0])
     os.makedirs(output_subdir, exist_ok=True)
 
     for index, row in df.iterrows():
-        fetch_and_save_pdf(row, output_subdir)
+        fetch_and_save_pdf(row, output_subdir, overwrite)
+
+    logging.info(f"Finished processing [{file_name}]")
 
 
-def fetch_and_save_pdf(row, output_dir):
+def fetch_and_save_pdf(row, output_dir, overwrite=False):
     sanitized_title = row['Title'].replace('/', '_').replace('"', "'")
     pdf_filename = os.path.join(output_dir, f"{sanitized_title}.pdf")
 
-    # Check if PDF already exists
-    if not os.path.exists(pdf_filename):
+    if not os.path.exists(pdf_filename) or overwrite:
         content_info = fetch_discourse_content_from_url(row['Link'])
         if content_info['content']:
             pdfkit.from_string(markdown_to_html(content_info['content']), pdf_filename)
@@ -32,23 +35,29 @@ def fetch_and_save_pdf(row, output_dir):
         else:
             logging.error(f"Failed to fetch content for URL: {row['Link']}")
     else:
-        pass
         logging.info(f"PDF already exists: [{'/'.join(pdf_filename.split('/')[-2:])}]")
 
 
-def process_csv_files_in_parallel(csv_dir, output_dir, thread_count):
-    csv_files = [f for f in os.listdir(csv_dir) if f.endswith('.csv')]
+def process_csv_files_in_parallel(csv_dir_or_file, output_dir, thread_count, overwrite=False):
+    if os.path.isfile(csv_dir_or_file):
+        # Single CSV file processing
+        csv_files = [csv_dir_or_file]
+    else:
+        # Multiple CSV files processing
+        csv_files = [os.path.join(csv_dir_or_file, f) for f in os.listdir(csv_dir_or_file) if f.endswith('.csv')]
+
     final_output_dir = os.path.join(output_dir, 'all_discourse_topics')
     os.makedirs(final_output_dir, exist_ok=True)
 
     with ThreadPoolExecutor(max_workers=thread_count) as executor:
-        executor.map(process_single_csv, csv_files, [csv_dir] * len(csv_files), [final_output_dir] * len(csv_files))
+        executor.map(process_single_csv, csv_files, [final_output_dir] * len(csv_files), [overwrite] * len(csv_files))
 
 
 if __name__ == "__main__":
-    csv_directory = f"{root_directory()}/data/links/articles"
+    # csv_path_or_directory = f"{root_directory()}/data/links/articles"  # Can be a directory or a single CSV file
+    csv_path_or_directory = f"{root_directory()}/data/links/articles/ethresearch_links.csv"  # Can be a directory or a single CSV file
     output_directory = f"{root_directory()}/data/articles_pdf_download"
-    thread_count = 20  # Adjust this to the desired number of parallel file processors
+    thread_count = 20  # Adjust based on your system capabilities
 
     os.environ['NUMEXPR_MAX_THREADS'] = str(thread_count)
-    process_csv_files_in_parallel(csv_directory, output_directory, thread_count)
+    process_csv_files_in_parallel(csv_path_or_directory, output_directory, thread_count, overwrite=True)
